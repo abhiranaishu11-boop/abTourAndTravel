70cf6a5095221a37758d8d6d1b8e0d93475d5772
Abhinav <abhiranaishu11@gmail.com>
2026-09-17 20:01:23 +0530

    Force Gmail SMTP over IPv4

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -1,7 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4,
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
